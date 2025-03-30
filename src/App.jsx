89a348c741bf6a7993cf94a401c7cca37c7b4947
sartaj <hajamsaj@gmail.com>
2025-03-30 17:41:27 +0530

    added Mobile Responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ const App = () => {
   return (
     <div>
       {/* <section className="h-screen bg-purple-600"> */}
-        <div className="grid grid-cols-12 ">
-          <div className="bg-red-300 col-span-4">Child 1</div>
-          <div className="bg-green-300 col-span-6" >Child 2</div>
-          <div className="bg-blue-300 col-span-2">Child 3</div>
+        <div className="grid grid-cols-1 sm:grid-cols-12 ">
+          <div className="bg-red-300 col-span-1 sm:col-span-5">Child 1</div>
+          <div className="bg-green-300 col-span-1 sm:col-span-5" >Child 2</div>
+          <div className="bg-blue-300 col-span-1 sm:col-span-2">Child 3</div>
         </div>
       {/* </section> */}
     </div>
