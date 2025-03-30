0e367edc2d53a1bb4ea751c0e145a188982f9d31
sartaj <hajamsaj@gmail.com>
2025-03-30 16:49:12 +0530

    added basic struct

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <section className="h-screen bg-purple-600">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam,
+          veniam? Ipsam sapiente itaque ea amet assumenda excepturi veniam
+          minima architecto! Praesentium in id dicta laudantium amet, iure
+          consequatur natus commodi!
+        </p>
+      </section>
+    </div>
+  );
+};
+
+export default App;
